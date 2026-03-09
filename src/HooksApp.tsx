4bcada29fc1f5df43ed3953ec4f03e4502388d57
Raul Garcia <rraulg5@gmail.com>
2026-03-09 02:05:24 -0600

    feat: integrate Tailwind CSS for styling and update HooksApp component
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Updated HooksApp component to use Tailwind CSS classes for styling.
    - Created a new CSS file to define a gradient background using Tailwind's utility classes.
    - Configured Vite to include Tailwind CSS in the build process.

--- src/HooksApp.tsx
+++ src/HooksApp.tsx
@@ -1,5 +1,7 @@
 export const HooksApp = () => {
   return (
-    <div>Hola Mundo</div>
-    );
-}
+    <div className='bg-gradient'>
+      <h1 class='text-3xl font-bold underline'>Hello world!</h1>
+    </div>
+  );
+};
